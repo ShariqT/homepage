c0729866aba5d3167704c5edb5219cbb62647204
Shariq Torres <shariq.torres@gmail.com>
2025-01-21 00:26:13 -0800

    edited metadata

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Shariq Torres | An engineer with the heart of an educator';
+export const SITE_DESCRIPTION = 'Shariq Torres, an experienced software engineer and developer relations professional. If you have a project that or need help reaching out to developers, then we should talk.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
