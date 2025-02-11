6566843880c42f1785f1a9614449ce794c3c7088
Shariq Torres <shariq.torres@gmail.com>
2025-02-10 16:45:36 -0800

    added new python project

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://shariqt.github.io/homepage',
+  base: '/homepage',
   integrations: [mdx(), sitemap(), tailwind()]
 });
